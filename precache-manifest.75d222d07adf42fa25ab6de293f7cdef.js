a42f181b18af64782655b4adb27012b92772a05f
Bethpen <64626802+Bethpen@users.noreply.github.com>
2022-03-07 11:28:09 +0100

    Updates

---
+++ precache-manifest.75d222d07adf42fa25ab6de293f7cdef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eafd0ef74bdf88146fbe446be1a43be",
+    "url": "/navbar/index.html"
+  },
+  {
+    "revision": "22c3dcc32e1e4c1185f1",
+    "url": "/navbar/static/css/main.beaf029b.chunk.css"
+  },
+  {
+    "revision": "96e76537d5de21102c5e",
+    "url": "/navbar/static/js/2.8bb64cab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/navbar/static/js/2.8bb64cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c3dcc32e1e4c1185f1",
+    "url": "/navbar/static/js/main.bdd800c8.chunk.js"
+  },
+  {
+    "revision": "4c4ded89cef8ec4aa9e1",
+    "url": "/navbar/static/js/runtime-main.5e9bde32.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/navbar/static/media/logo.2bb7da65.svg"
+  }
+]);
